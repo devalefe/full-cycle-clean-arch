6b20f7ec3d034d567e6cc06079f5a1ebf709acf7
Alefe F Oliveira <dev.alefe@gmail.com>
2023-09-13 13:28:24 -0300

    feature: ensure CreateProductUseCase return product if product is created

--- src/domain/product/repository/product-repository.interface.ts
+++ src/domain/product/repository/product-repository.interface.ts
@@ -1,5 +1,6 @@
 import RepositoryInterface from "../../@shared/repository/repository-interface";
-import Product from "../entity/product";
+import ProductInterface from "../entity/product.interface";
+// import Product from "../entity/product";
 
 export default interface ProductRepositoryInterface
-  extends RepositoryInterface<Product> {}
+  extends RepositoryInterface<ProductInterface> {}
---
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -0,0 +1,32 @@
+import CreateProductUseCase from "./create.product.usecase";
+
+const input = {
+  id: "1",
+  type: "a",
+  name: "Product 1",
+  price: 100,
+}
+
+const MockRepository = () => {
+  return {
+    find: jest.fn(),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe('Unit test create product use case', () => {
+  it('Should create a product', async () => {
+    const productRepository = MockRepository();
+    const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+    const output = await productCreateUseCase.execute(input);
+
+    expect(output).toEqual({
+      id: expect.any(String),
+      name: input.name,
+      price: input.price,
+    })
+  });
+});
