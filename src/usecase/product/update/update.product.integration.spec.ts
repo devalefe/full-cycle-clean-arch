c3a0ddbda5adaa1227145b0923c73048c26a90a7
Alefe F Oliveira <dev.alefe@gmail.com>
2023-09-14 12:32:30 -0300

    reafctor: remove unused dependecy

--- src/usecase/product/update/update.product.integration.spec.ts
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -2,7 +2,6 @@ import { Sequelize } from "sequelize-typescript";
 import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
 import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
 import UpdateProductUseCase from "./update.product.usecase";
-import CreateProductUseCase from "../create/create.product.usecase";
 import Product from "../../../domain/product/entity/product";
 
 describe('Test update product use case', () => {
